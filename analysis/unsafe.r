87d8b875647cafacf926146bcd2e8726590b13aa
Luigi <a>
2015-02-23 11:12:01 +0100

    Inverted dependencies.

--- analysis/unsafe.r
+++ analysis/unsafe.r
@@ -1,15 +1,12 @@
 
 library(ggplot2)
-library(reshape2)
 library(tools)
-library(scales)
-library(xtable)
-suppressMessages(library(gdata))
+library(reshape2)
 
 printf <- function(format, ...) print(sprintf(format, ...));
 
 save.plot <- function(plot, d, s, w=12, h=8) {
-  path <- sprintf('build/%s-%s.pdf', d, s)
+  path <- sprintf('build/%s-plot-%s.pdf', d, s)
   printf("Saving plot %s to %s", s, path)
   pdf(file=path, paper='special', width=w, height=h, pointsize=12)
   print(plot)
@@ -32,7 +29,7 @@ path <- file_path_sans_ext(csvfilename);
 df.maven <- (function() {
   csv.maven <- read.csv(csvfilename, strip.white=TRUE, sep=',', header=TRUE);
   df.maven <- merge(csv.maven, df.methods, by.x="name", by.y="method", all.x=FALSE);
-  df.maven$id <- factor(paste(df.maven$groupId, df.maven$artifactId, sep='_'));
+  df.maven$id <- factor(paste(df.maven$groupId, df.maven$artifactId, sep=':'));
 
   noartifacts <- nrow(dcast(df.maven, groupId+artifactId~name, value.var='name', fun.aggregate=length));
   nogroups <- nrow(dcast(df.maven, groupId~name, value.var='name', fun.aggregate=length));
@@ -43,18 +40,29 @@ df.maven <- (function() {
   df.maven
 })();
 
-dcast(df.maven, groupId+artifactId+id~name, value.var='name', fun.aggregate=length);
-
 save.plot(ggplot(df.maven, aes(x=name))+geom_bar(stat="bin")+facet_grid(.~group, space='free_x', scales="free_x")+
             theme(axis.text.x=element_text(angle=62, hjust=1), axis.text.y=element_text(angle=90, hjust=1), 
                   axis.title.x=element_text(angle=180),
                   legend.box="horizontal", legend.position="top", 
                   legend.title=element_blank(),strip.text.x=element_text(angle=90))+
-            labs(x="sun.misc.Unsafe methods", y = "# call sites"), path, "plot-cs", w=15, h=6);
+            labs(x="sun.misc.Unsafe methods", y = "# call sites"), path, 'cs', w=15, h=6);
 
 gaid <- 'org.python_jython'
-for (gaid in levels(df.maven$id)) {
+for (gaid in head(levels(df.maven$id), 5)) {
   save.plot(ggplot(subset(df.maven, id==gaid), aes(x=name))+geom_bar(stat="bin")+facet_grid(.~group, space='free_x', scales="free_x")+
             theme(axis.text.x=element_text(angle=45, hjust=1))+ 
-            labs(x="sun.misc.Unsafe methods", y = "# call sites"), path, paste('plot-cs', gaid, sep='-'));
+            labs(x="sun.misc.Unsafe methods", y = "# call sites"), path, paste('cs', gaid, sep='-'));
 }
+
+
+csv.invdeps <- read.csv('maven-invdeps.csv', strip.white=TRUE, sep=',', header=TRUE);
+df.invdeps <- csv.invdeps[with(csv.invdeps, order(-depCount) ), ]
+df.invdeps$rank <- c(1:nrow(df.invdeps))
+df <- dcast(df.maven, id~., value.var='name', fun.aggregate=length);
+df <- merge(df, df.invdeps, by.x='id', by.y='depId', all.x=TRUE);
+df <- df[with(df, order(rank) ), ]
+
+save.plot(ggplot(df, aes(x=rank, y='Ranking'))+
+            geom_vline(aes(xintercept=rank, x=rank, y='Ranking'))+
+            theme(axis.text.y=element_blank())+labs(x="Ranking", y = "Artifacts"), 
+          path, 'ranking', h=2, w=8);
--- maven/src-test/ch/usi/inf/sape/unsafe/maven/test/MirrorTest.java
+++ maven/src-test/ch/usi/inf/sape/unsafe/maven/test/MirrorTest.java
@@ -1,6 +1,17 @@
 package ch.usi.inf.sape.unsafe.maven.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -22,4 +33,67 @@ public class MirrorTest {
 		assertEquals("ibiblio", m.id);
 	}
 
+	private static Set<String> doDeps(String did, Map<String, Set<String>> ds,
+			Map<String, Set<String>> bs) {
+
+		Set<String> s = bs.get(did);
+
+		if (s != null && s.size() == 0) {
+			s.add(did);
+
+			for (String id : ds.get(did)) {
+				Set<String> t = doDeps(id, ds, bs);
+				if (t != null) {
+					s.addAll(t);
+				}
+			}
+		}
+
+		return s;
+	}
+
+	@Test
+	public void depsTest() throws IOException {
+		Map<String, Set<String>> ds = new HashMap<String, Set<String>>();
+		Map<String, Set<String>> bs = new HashMap<String, Set<String>>();
+
+		File file = new File("db/maven-depgraph.csv");
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			reader.readLine();
+
+			String text;
+			while ((text = reader.readLine()) != null) {
+				String[] fs = text.split(",");
+
+				if (fs.length >= 4) {
+					String id = fs[0].trim() + ":" + fs[1].trim();
+					String did = fs[2].trim() + ":" + fs[3].trim();
+
+					Set<String> s = ds.get(did);
+					if (s == null) {
+						s = new HashSet<String>();
+						ds.put(did, s);
+
+						bs.put(did, new HashSet<String>());
+					}
+
+					s.add(id);
+				}
+			}
+
+			System.out.format("Artifacts %d\n", bs.size());
+
+			for (String did : bs.keySet()) {
+				doDeps(did, ds, bs);
+			}
+
+			try (PrintStream out = new PrintStream("db/maven-invdeps.csv")) {
+				out.println("depId, depCount");
+				for (Entry<String, Set<String>> e : bs.entrySet()) {
+					out.format("%s, %d\n", e.getKey(), e.getValue().size());
+				}
+			}
+		}
+	}
 }
