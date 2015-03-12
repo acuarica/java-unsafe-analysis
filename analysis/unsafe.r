607faeef0dcb79b757a660ee1a9597d60f626aa7
Luigi <a>
2015-03-12 11:05:58 +0100

    Progress.

--- analysis/unsafe.r
+++ analysis/unsafe.r
@@ -46,19 +46,33 @@ df.methods <- (function() {
 })();
 
 df.maven <- (function() {
+  
+  fcls <- function(cls) {    
+    if (substr(cls, 1, 1) == '$') {
+      cls <- substring(cls, 2);
+    }
+    
+    fqnpath <- strsplit(cls, "\\$")[[1]];
+    
+    sprintf("%s*", fqnpath[1]);
+  }
+  
   csv.maven <- read.csv(csvfilename, strip.white=TRUE, sep=',', header=TRUE);
   df.maven <- merge(csv.maven, df.methods, by.x="name", by.y="method", all.x=FALSE);
   df.maven$id <- factor(paste(df.maven$groupId, df.maven$artifactId, sep=':'));
 
   df.maven$package <- NA;
   df.maven$class = NA;
+  df.maven$classunit <- NA;
   for (i in 1:nrow(df.maven)) {
-    className <- as.character(df.maven$className[i]);
-    a <- strsplit(className, '/')[[1]]
+    fqn <- as.character(df.maven$className[i]);
+    a <- strsplit(fqn, '/')[[1]];
     df.maven$package[i] <- paste(a[1:length(a)-1], collapse='/');
     df.maven$class[i] <- a[length(a)];
+    df.maven$classunit[i] <- fcls(df.maven$class[i]);
   }
   df.maven$class <- factor(df.maven$class);
+  df.maven$fid <- factor(paste(df.maven$className, df.maven$id, sep='@'));
 
   df.maven
 })();
@@ -103,7 +117,7 @@ df.most.used.artifacts.w.unsafe <- (function() {
   df;
 })();
 
-cluster.sort <- (function() {
+cluster.groups <- (function() {
   df <- dcast(df.maven, id~name, value.var='name', fun.aggregate=length);
   rownames(df) <- df$id
   df$id <- NULL
@@ -113,11 +127,40 @@ cluster.sort <- (function() {
   
   save.plot.open(csvfilename, 'cluster', w=64, h=64);
   plot(fit);
-  groups <- cutree(fit, k=5);
-  rect.hclust(fit, k=4, border="red");
+  rect.hclust(fit, k=20, border="blue");
+  rect.hclust(fit, h=100, border="red");
+  rect.hclust(fit, h=75, border="green");
+  rect.hclust(fit, h=50, border="yellow");
+  rect.hclust(fit, h=25, border="gray");
   save.plot.close();
   
-  row.names(df[fit$order,]);
+  #row.names(df[fit$order,]);
+  groups <- cutree(fit, h=25);
+  #levels(as.factor(groups));
+  groups;
+})();
+
+cluster.by.class <- (function() {
+  df <- dcast(df.maven, className~name, value.var='name', fun.aggregate=length);
+  rownames(df) <- df$fid
+  df$fid <- NULL
+  
+  d <- dist(df, method = "euclidean");
+  fit <- hclust(d, method="ward.D");
+  
+  save.plot.open(csvfilename, 'cluster2', w=256, h=64);
+  plot(fit);
+  rect.hclust(fit, k=20, border="blue");
+  rect.hclust(fit, h=100, border="red");
+  rect.hclust(fit, h=75, border="green");
+  rect.hclust(fit, h=50, border="yellow");
+  rect.hclust(fit, h=25, border="gray");
+  save.plot.close();
+  
+  #row.names(df[fit$order,]);
+  groups <- cutree(fit, h=25);
+  #levels(as.factor(groups));
+  groups;
 })();
 
 (function() {
@@ -133,6 +176,8 @@ cluster.sort <- (function() {
                     legend.title=element_blank(),strip.text.x=element_text(angle=90))+
               labs(x="sun.misc.Unsafe methods", y = "# call sites"), csvfilename, 'cs-all-vertical-pre', w=15, h=6);  
 })();
+
+df <- dcast(df.maven, classunit~., fun.aggregate=length);
 
 df <- dcast(df.maven, class+className~., fun.aggregate=length);
 df <- dcast(df, class~., fun.aggregate=length);
@@ -161,12 +206,16 @@ for (i in df$groupId) {
 }
 save.plot.close();
 
-save.plot.open(csvfilename, 'cs-artifacts');
-for (i in cluster.sort) {
-  printf('Processing artifact %s...', i);
-  print(ggplot(subset(df.maven, id==i), aes(x=name, fill=className))+geom_bar(stat="bin")+
+for (i in levels(as.factor(cluster.groups))) {
+  i <- "2";
+  save.plot.open(csvfilename, sprintf('cs-artifacts-group%s', i));
+  arts <- names(cluster.groups[cluster.groups==as.numeric(i)]);
+  for (art in arts) {
+    printf('Processing artifact %s...', art);
+    print(ggplot(subset(df.maven, id==art), aes(x=name, fill=class))+geom_bar(stat="bin")+
       facet_grid(.~group, space='free_x', scales="free_x")+
       theme(axis.text.x=element_text(angle=45, hjust=1))+
-      labs(x="sun.misc.Unsafe methods", y = "# call sites"));
+      labs(x="sun.misc.Unsafe methods", y = "# call sites", title=sprintf("Plot of << %s >> by class name", art)));
+  }
+  save.plot.close();
 }
-save.plot.close();
