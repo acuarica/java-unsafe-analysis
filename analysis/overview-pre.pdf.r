7d6f04a57872735a211a705bd8b7c3e4bfe76f32
Luigi <a>
2015-03-23 21:46:11 +0100

    Progress.
    Former-commit-id: 9947f986ad70945d3d7f0a7ea05e458f04a194ed

--- analysis/overview-pre.pdf.r
+++ analysis/overview-pre.pdf.r
@@ -4,22 +4,30 @@ library(ggplot2)
 
 source('utils.r')
 
-df = load.csv('build/cs.csv');
-df = dcast(df, name~'cs', value.var='cs', fun.aggregate=sum);
-df = merge(df, df.methods, by.x='name', by.y='method', all.x=TRUE, all.y=TRUE);
-df[is.na(df$cs),]$cs = 0;
+replacefield = function(df, fname, toname) {
+    df[df$name == fname,]$name = toname;
+    df;
+}
+
+csv = load.csv('build/cs.csv');
+csv = merge(csv, df.methods, by.x='name', by.y='method', all.x=TRUE, all.y=TRUE);
 
 plotoverview = function(df, outfile) {
+  df = dcast(df, name+group~'cs', value.var='cs', fun.aggregate=sum);
+  #df = merge(df, df.methods, by.x='name', by.y='method', all.x=TRUE, all.y=TRUE);
+  df[is.na(df$cs),]$cs = 0;
+
   p = ggplot(df, aes(x=name, y=cs))+
     geom_bar(stat="identity")+
     facet_grid(.~group, space='free_x', scales="free_x")+
-    theme(axis.text.x=element_text(size=9, angle=90, hjust=1, vjust=0.2),
+    theme(axis.text.x=element_text(size=10, angle=90, hjust=1, vjust=0.2),
           axis.text.y=element_text(angle=90, hjust=1),
-          axis.title.x=element_text(angle=180),
+          #axis.title.x=element_text(angle=180),
+          axis.title.x=element_blank(),
           legend.box="horizontal", 
           legend.position="top", 
           legend.title=element_blank(),
-          strip.text.x=element_text(size=8, angle=90),
+          strip.text.x=element_text(size=11, angle=90),
           
           #panel.background=element_rect(fill='white'),
             #plot.background=element_rect(color='green'),
@@ -34,5 +42,58 @@ plotoverview = function(df, outfile) {
   save.plot(p, outfile, w=15.47, h=6);
 }
 
+df = csv;
 plotoverview(df, suffixfile(outfile, 'all'));
-plotoverview(df[df$access=='public' & df$name != 'getUnsafe',], outfile);
+
+df = csv;
+df = df[df$access=='public' & df$name != 'getUnsafe',];
+plotoverview(df, outfile);
+
+df = csv;
+df$name = as.character(df$name);
+df = replacefield(df, 'ADDRESS_SIZE', 'addressSize');
+df = replacefield(df, 'ARRAY_BOOLEAN_BASE_OFFSET', 'arrayBaseOffset');
+df = replacefield(df, 'ARRAY_BOOLEAN_INDEX_SCALE', 'arrayIndexScale');
+df = replacefield(df, 'ARRAY_BYTE_BASE_OFFSET', 'arrayBaseOffset');
+df = replacefield(df, 'ARRAY_BYTE_INDEX_SCALE', 'arrayIndexScale');
+df = replacefield(df, 'ARRAY_CHAR_BASE_OFFSET', 'arrayBaseOffset');
+df = replacefield(df, 'ARRAY_CHAR_INDEX_SCALE', 'arrayIndexScale');
+df = replacefield(df, 'ARRAY_DOUBLE_BASE_OFFSET', 'arrayBaseOffset');
+df = replacefield(df, 'ARRAY_DOUBLE_INDEX_SCALE', 'arrayIndexScale');
+df = replacefield(df, 'ARRAY_FLOAT_BASE_OFFSET', 'arrayBaseOffset');
+df = replacefield(df, 'ARRAY_FLOAT_INDEX_SCALE', 'arrayIndexScale');
+df = replacefield(df, 'ARRAY_INT_BASE_OFFSET', 'arrayBaseOffset');
+df = replacefield(df, 'ARRAY_INT_INDEX_SCALE', 'arrayIndexScale');
+df = replacefield(df, 'ARRAY_LONG_BASE_OFFSET', 'arrayBaseOffset');
+df = replacefield(df, 'ARRAY_LONG_INDEX_SCALE', 'arrayIndexScale');
+df = replacefield(df, 'ARRAY_OBJECT_BASE_OFFSET', 'arrayBaseOffset');
+df = replacefield(df, 'ARRAY_OBJECT_INDEX_SCALE', 'arrayIndexScale');
+df = replacefield(df, 'ARRAY_SHORT_BASE_OFFSET', 'arrayBaseOffset');
+df = replacefield(df, 'ARRAY_SHORT_INDEX_SCALE', 'arrayIndexScale');
+df$name = factor(df$name);
+
+exclude = c(
+  'getUnsafe', 
+'ADDRESS_SIZE', 
+'ARRAY_BOOLEAN_BASE_OFFSET', 
+'ARRAY_BOOLEAN_INDEX_SCALE', 
+'ARRAY_BYTE_BASE_OFFSET', 
+'ARRAY_BYTE_INDEX_SCALE', 
+'ARRAY_CHAR_BASE_OFFSET', 
+'ARRAY_CHAR_INDEX_SCALE', 
+'ARRAY_DOUBLE_BASE_OFFSET', 
+'ARRAY_DOUBLE_INDEX_SCALE', 
+'ARRAY_FLOAT_BASE_OFFSET', 
+'ARRAY_FLOAT_INDEX_SCALE', 
+'ARRAY_INT_BASE_OFFSET', 
+'ARRAY_INT_INDEX_SCALE', 
+'ARRAY_LONG_BASE_OFFSET', 
+'ARRAY_LONG_INDEX_SCALE', 
+'ARRAY_OBJECT_BASE_OFFSET', 
+'ARRAY_OBJECT_INDEX_SCALE', 
+'ARRAY_SHORT_BASE_OFFSET', 
+'ARRAY_SHORT_INDEX_SCALE'
+);
+
+df = df[df$access=='public' & !(df$name %in% exclude),];
+plotoverview(df, suffixfile(outfile, 'nofields'));
---
+++ analysis/overview-so-pre.pdf.r
@@ -0,0 +1,71 @@
+
+library(reshape2)
+library(ggplot2)
+
+source('utils.r')
+
+replacename = function(df, name, offheapdesc) {
+  df[df$method == sprintf('%s (Heap)', name),]$method = name;
+  df[df$method == sprintf('%s (Off-Heap)', name),]$method = name;  
+  df;
+}
+
+df = df.methods;
+df$method = as.character(df$method);
+df = replacename(df, 'copyMemory', '(JJJ)V');
+df = replacename(df, 'setMemory', '(JJB)V');
+df = replacename(df, 'getBoolean', '(J)B');
+df = replacename(df, 'getByte', '(J)B');
+df = replacename(df, 'getChar', '(J)C');
+df = replacename(df, 'getDouble', '(J)D');
+df = replacename(df, 'getFloat', '(J)F');
+df = replacename(df, 'getInt', '(J)I');
+df = replacename(df, 'getLong', '(J)J');
+df = replacename(df, 'getShort', '(J)S');
+df = replacename(df, 'putBoolean', '(JB)V');
+df = replacename(df, 'putByte', '(JB)V');
+df = replacename(df, 'putChar', '(JC)V');
+df = replacename(df, 'putDouble', '(JD)V');
+df = replacename(df, 'putFloat', '(JF)V');
+df = replacename(df, 'putInt', '(JI)V');
+df = replacename(df, 'putLong', '(JJ)V');
+df = replacename(df, 'putShort', '(JS)V');
+
+df$group = as.character(df$group);
+df[df$group == 'Heap Get',]$group = 'Get';
+df[df$group == 'Off-Heap Get',]$group = 'Get';
+df[df$group == 'Heap Put',]$group = 'Put';
+df[df$group == 'Off-Heap Put',]$group = 'Put';
+
+so = load.csv('csv/so-method-usages.csv');
+colnames(so) = c('method','Usages in Questions only', 'Usages in Answers only', 'Usages in both');
+
+so = merge(so, df, by.x='method', by.y='method', all.x=TRUE, all.y=FALSE);
+so = melt(so, id=c('method', 'access', 'group'));
+
+p = ggplot(so, aes(x=method, y=value, fill=variable))+
+  geom_bar(stat="identity")+
+  facet_grid(.~group, space='free_x', scales="free_x")+
+  theme(axis.text.x=element_text(size=10, angle=90, hjust=1, vjust=0.2),
+        axis.text.y=element_text(angle=90, hjust=1),
+        #axis.title.x=element_text(angle=180),
+        axis.title.x=element_blank(),
+        legend.box="horizontal", 
+        legend.position="none", 
+        #legend.text =element_text(angle=90, vjust=0),
+        #legend.text.align=
+        legend.title=element_blank(),
+        strip.text.x=element_text(size=11, angle=90),
+        
+        #panel.background=element_rect(fill='white'),
+        #plot.background=element_rect(color='green'),
+        #panel.border=element_rect(color='black'),
+        
+        panel.grid.major=element_line(),
+        panel.grid.minor=element_blank()
+        #axis.ticks=element_blank()
+  )+
+  labs(x="sun.misc.Unsafe members", y = "# matches");
+
+#outfile = 'build/overview-pre.pdf';
+save.plot(p, outfile, w=14, h=6);
