c07ebde2c39ad40b657f46587dda371c05fa04ea
Luigi <a>
2015-03-24 16:20:32 +0100

    Change dep plot.
    Former-commit-id: 5c9717c448d903c9f7f4fb2a316bea3b6f2ee15b

--- analysis/summary.pdf.r
+++ analysis/summary.pdf.r
@@ -8,6 +8,7 @@ source('utils.r')
 
 df.maven <- read.csv('build/cs.csv', strip.white=TRUE, sep=',', header=TRUE);
 
+f = 'csv/maven-invdeps-production.csv';
 invdeps <- function(f) {
   csv.invdeps <- read.csv(f, strip.white=TRUE, sep=',', header=TRUE);
   df.invdeps <- csv.invdeps[with(csv.invdeps, order(-depCount) ), ]
@@ -15,15 +16,22 @@ invdeps <- function(f) {
   df <- dcast(df.maven, id~., value.var='name', fun.aggregate=length);
   df <- merge(df, df.invdeps, by.x='id', by.y='depId', all.x=TRUE);
   df <- df[with(df, order(rank) ), ]
-    
+
   grid.newpage();
   grid.table(df.invdeps[1:20,], show.rownames=FALSE);
   
   grid.newpage();
   grid.table(df[1:20,c('id', 'depCount', 'rank')], show.rownames=FALSE);
+
+  df$unsafe = TRUE;
+  df$depCount = NULL;
+  df.all = merge(df.invdeps, df, by.x='depId', by.y='id', all.x=TRUE, all.y=TRUE);
+  #hola=df.all[!is.na(df.all$unsafe) & df.all$unsafe == TRUE,]
+  df.all[is.na(df.all$unsafe),]$unsafe = FALSE;
   
-  ggplot(subset(df, !is.na(depCount)), aes(x=rank))+
-    geom_histogram(binwidth=1000)+
+  ggplot(subset(df.all, !is.na(depCount) & depCount > 1000), aes(x=depCount, fill=unsafe))+
+    geom_histogram(binwidth=500)+
+    #geom_density()+
     theme()+
     labs(x="Ranking", y = "Artifacts");
 };
