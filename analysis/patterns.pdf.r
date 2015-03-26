e92cce7a2c3621c8195ff854396d3412392a4a28
Luigi <a>
2015-03-26 01:08:17 +0100

    Added more stats.
    Former-commit-id: e930818187e558688ae44f2bfccdc97cb743668e

--- analysis/patterns.pdf.r
+++ analysis/patterns.pdf.r
@@ -16,57 +16,81 @@ df.deps = load.csv('build/deps-prod.csv');
 df.cu = dcast(df.maven, id+package+classunit~name, value.var='cs', fun.aggregate=sum);
 df.clones = dcast(df.cu, classunit~'clones', value.var='id', fun.aggregate=length);
 
-levs = function(col) {
-  levels(factor(col));
-}
+#levs = function(col) {
+#  levels(factor(col));
+#}
 
 depsbyarts = function(arts) {
-  length(levs(df.deps[df.deps$depId %in% arts,]$depCount));
-}
-
-deps = function(classes) {
-  unsart = levs(df.maven[df.maven$classunit %in% classes, ]$id);
-  count = length(levs(df.deps[df.deps$depId %in% unsart,]$depCount));
-  count;
-}
-
-clones = function(classes) {
-  df = df.clones[df.clones$classunit %in% classes, ];
-  sum(df$clones);
+  length(unique(df.deps[df.deps$depId %in% arts,]$depCount)) + length(arts);
 }
 
 mostdeps = function(arts) {
   df = df.deps[df.deps$depId %in% arts,];
   df = dcast(df, depId~'value', value.var='depCount', fun.aggregate=length);
   df = df[order(-df$value),];
-  as.character(paste(df$depId[1:5], collapse='\n') );
+  
+  df$depId[1:10];
+  #as.character(paste(df$depId[1:10], collapse='\n') );
+  #paste( c(as.character(df$depId), as.character(arts))[1:10], collapse='\n') ;
 }
 
-row = function(name, classes) {
-  list(name, length(classes), clones(classes), deps(classes));
-}
+garts = list();
 
-rows = list();
-
-i = 1;
-rid = 1;
+patterns = patterns[patterns != '.impl'];
 for (i in 1:length(patterns)) {
   pattern = patterns[i];
-  
-  if (substr(pattern, 1, 1) != '.') {
-    arts = csv.comments[grepl(pattern, csv.comments$pattern),]$id
-    rows[[rid]] = list(pattern, length(arts), depsbyarts(arts), mostdeps(arts));
-    rid = rid + 1;
-  }
+  arts = csv.comments[grepl(pattern, csv.comments$pattern),]$id;
+  garts[[length(garts)+1]] = list(pattern, arts);
 }
+
+garts[[length(garts)+1]] = (function() {
+  gs = c('Fence', 'Volatile Get', 'Volatile Put');
+  ms = as.character(subset(df.methods, group %in% gs)$method);
+  arts = unique(subset(df.maven, name %in% ms)$id);
+  list('fence', arts);
+})();
+
+garts[[length(garts)+1]] = (function() {
+  arts = unique(subset(df.maven, name %in% c('park', 'unpark'))$id);
+  list('park2', arts);
+})();
+
+garts[[length(garts)+1]] = (function() {
+  arts = unique(subset(df.maven, name %in% c('monitorEnter', 'monitorExit'))$id);
+  list('monitor2', arts);
+})();
+
+rows = list();
+txt = '';
+
+for (i in 1:length(garts)) {
+  pattern = garts[[i]][[1]];
+  arts = garts[[i]][[2]];
+
+  len = length(arts);
+  deps = depsbyarts(arts);
+  most = mostdeps(arts);
+  
+  mosttext = as.character(paste(most, collapse='\n') );
+  rows[[ length(rows)+1 ]] = list(pattern, len, deps, mosttext);
+  
+  txt = sprintf('%s\\newcommand{\\%slen}{%s}\n', txt, pattern, len);
+  txt = sprintf('%s\\newcommand{\\%sdeps}{%s}\n', txt, pattern, deps);
+  txt = sprintf('%s\\newcommand{\\%smost}{%s}\n', txt, pattern, mosttext);
+  txt = sprintf('%s\n', txt);
+}
+
+f = file('build/patterns-stats.tex');
+writeLines(txt, f);
+close(f);
 
 df = data.frame(matrix(unlist(rows), ncol=4, byrow=T));
 colnames(df) = c('pattern', 'artifact.count', 'usedby.count', 'most.deps');
 df$usedby.count = as.numeric(as.character(df$usedby.count));
 df = df[order(-df$usedby.count),];
-rownames(df) = 1:(rid-1);
+rownames(df) = 1:length(rows);
 
-save.plot.open(outfile, w=8, h=17);
+save.plot.open(outfile, w=10, h=length(rows)*2.5);
 grid.newpage();
 grid.table(df);
 save.plot.close();
