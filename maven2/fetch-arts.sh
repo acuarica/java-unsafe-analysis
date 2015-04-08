a60942ee6d6c98e87976b045c493c9080f7a8144
Luigi <a>
2015-04-08 18:07:12 +0200

    Progress.
    Former-commit-id: 4e7d6a435c4e8a2282e2347782630f55b9d8b89c

--- maven2/fetch-arts.sh
+++ maven2/fetch-arts.sh
@@ -1,2 +1,2 @@
 
-aria2c --dir=db/repo --max-concurrent-downloads=8 --auto-file-renaming=false --input-file=db/download.list
+aria2c --dir=db/repo --max-concurrent-downloads=16 --auto-file-renaming=false --input-file=db/download.list
