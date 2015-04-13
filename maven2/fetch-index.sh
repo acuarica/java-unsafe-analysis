fbdba1a32e246a417b4981da082ea739a045079c
Luigi <a>
2015-04-13 14:57:58 +0200

    Fixed index path.
    Former-commit-id: e6f0271da80e4d34f96eb25a8ec9b4424b4cb53b

--- maven2/fetch-index.sh
+++ maven2/fetch-index.sh
@@ -3,5 +3,5 @@ source config.sh
 
 mkdir -p $DB/index
 
-curl http://mirrors.ibiblio.org/maven2/.index/nexus-maven-repository-index.gz -o $DB/index/nexus-maven-repository-index.gz
-gunzip $DB/index/nexus-maven-repository-index.gz
+curl http://mirrors.ibiblio.org/maven2/.index/nexus-maven-repository-index.gz -o $DB/nexus-maven-repository-index.gz
+gunzip $DB/nexus-maven-repository-index.gz
