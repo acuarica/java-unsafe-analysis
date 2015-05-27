0fcb245e77a9c0b5c556105d319452175e0440c5
Luigi <a>
2015-05-28 00:46:34 +0200

    Progress.
    Former-commit-id: b9db689da4eccbe4437bc3df0e906b788f0c56fd

--- src/ch/usi/inf/sape/unsafeanalysis/BuildUriList.java
+++ src/ch/usi/inf/sape/unsafeanalysis/BuildUriList.java
@@ -2,6 +2,7 @@ package ch.usi.inf.sape.unsafeanalysis;
 
 import java.io.File;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 import org.apache.log4j.Logger;
 
@@ -15,31 +16,45 @@ public class BuildUriList {
 	private static final Logger logger = Logger.getLogger(BuildUriList.class);
 
 	public static void main(String[] args) throws Exception {
-		if (args.length < 3) {
+		if (args.length < 4) {
 			throw new Exception(
-					"Invalid arguments: needed index path, uri list, and at least on mirror");
+					"Invalid arguments: needed index path, uri list, number of artifacts to download (or - for no limit), and at least on mirror");
 		}
 
 		String indexPath = args[0];
 		String uriListPath = args[1];
+		String arts = args[2];
+		Integer noArtsToDownload = "-".equals(arts) ? null : Integer
+				.parseInt(arts);
+
+		String[] mirrors = Arrays.copyOfRange(args, 3, args.length);
 
 		logger.info("Index: " + indexPath);
 		logger.info("URI list: " + uriListPath);
+		logger.info("# artifact to download: " + noArtsToDownload);
 
 		NexusIndexParser nip = new NexusIndexParser(indexPath);
 		MavenIndex index = MavenIndexBuilder.build(nip);
 
 		new File(new File(uriListPath).getParent()).mkdirs();
-		try (PrintStream out = new PrintStream(uriListPath)) {
-			for (MavenArtifact a : index) {
-				for (int i = 2; i < args.length; i++) {
-					String mirror = args[i];
 
+		try (PrintStream out = new PrintStream(uriListPath)) {
+			int noArtifacts = 0;
+
+			for (MavenArtifact a : index) {
+				for (String mirror : mirrors) {
 					out.format("%s/%s\t", mirror, a.getPath());
 				}
 
 				out.println();
 				out.format("\tout=%s\n", a.getPath());
+
+				noArtifacts++;
+
+				if (noArtsToDownload != null
+						&& noArtsToDownload.intValue() == noArtifacts) {
+					break;
+				}
 			}
 		}
 	}
