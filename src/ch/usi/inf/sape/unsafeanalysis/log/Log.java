8798f58fa94730fbc85c656ebb4581b52215135c
Luigi <a>
2015-06-03 10:41:08 +0200

    Added some tests.
    Former-commit-id: df3e727d8b130619586e45aa8df8a55217f06c27

--- src/ch/usi/inf/sape/unsafeanalysis/log/Log.java
+++ src/ch/usi/inf/sape/unsafeanalysis/log/Log.java
@@ -2,6 +2,11 @@ package ch.usi.inf.sape.unsafeanalysis.log;
 
 import java.io.PrintStream;
 
+/**
+ * Wrapper around a PrintStream to serve as a log. 
+ * @author Luis Mastrangelo
+ *
+ */
 public class Log {
 
 	private final PrintStream out;
