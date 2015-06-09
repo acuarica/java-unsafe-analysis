ac47b37e2e372fb67391f102f8c11f3919c4e310
Luigi <a>
2015-06-09 12:52:40 +0200

    Changed maximum heap size for JVM from 4gb to 2gb because 4 is not allowed in 32bits OSes.
    Former-commit-id: 0d2f7d277865beb78a5fa1e170bd301bcc146a9d

--- stackoverflow/step1-default.sh
+++ stackoverflow/step1-default.sh
@@ -1,4 +1,4 @@
 if [ ! -d "results" ]; then
   mkdir results
 fi
-java -Xmx4G -cp unsafe-analysis-0.0.1-SNAPSHOT-jar-with-dependencies.jar ch.usi.inf.reveal.unsafe_analysis.CandidatesExtractor ./data/json ./results/candidates.csv
+java -Xmx2G -cp unsafe-analysis-0.0.1-SNAPSHOT-jar-with-dependencies.jar ch.usi.inf.reveal.unsafe_analysis.CandidatesExtractor ./data/json ./results/candidates.csv
--- stackoverflow/step2-default.sh
+++ stackoverflow/step2-default.sh
@@ -1,1 +1,1 @@
-java -Xmx4G -cp unsafe-analysis-0.0.1-SNAPSHOT-jar-with-dependencies.jar ch.usi.inf.reveal.unsafe_analysis.UnsafeUsageExtractor ./data/json ./results/candidates.csv ./results/intermediate.csv
+java -Xmx2G -cp unsafe-analysis-0.0.1-SNAPSHOT-jar-with-dependencies.jar ch.usi.inf.reveal.unsafe_analysis.UnsafeUsageExtractor ./data/json ./results/candidates.csv ./results/intermediate.csv
