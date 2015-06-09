ebd9baf3ce8bf397b94e9f364938e97d209b8a0d
Luca Ponzanelli <luca.ponzanelli@usi.ch>
2015-06-09 10:33:02 +0200

    so unsafe analysis scripts
    Former-commit-id: 92c3b9df491a4ab658cca2f168a3db3f78849015

---
+++ stackoverflow/step1-default.sh
@@ -0,0 +1,4 @@
+if [ ! -d "results" ]; then
+  mkdir results
+fi
+java -Xmx4G -cp unsafe-analysis-0.0.1-SNAPSHOT-jar-with-dependencies.jar ch.usi.inf.reveal.unsafe_analysis.CandidatesExtractor ./data ./results/candidates.csv
