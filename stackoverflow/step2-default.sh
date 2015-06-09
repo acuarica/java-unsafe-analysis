0d26444200f032b351e95ffbf054cd22313b396f
Andrea Mocci <andrea.mocci@gmail.com>
2015-06-09 10:43:00 +0200

    Little changes
    Former-commit-id: 2d9b84234dd004ae7bab12d83f753b3382e69f91

--- stackoverflow/step2-default.sh
+++ stackoverflow/step2-default.sh
@@ -1,1 +1,1 @@
-java -Xmx4G -cp unsafe-analysis-0.0.1-SNAPSHOT-jar-with-dependencies.jar ch.usi.inf.reveal.unsafe_analysis.UnsafeUsageExtractor ./data ./results/candidates.csv ./results/intermediate.csv
+java -Xmx4G -cp unsafe-analysis-0.0.1-SNAPSHOT-jar-with-dependencies.jar ch.usi.inf.reveal.unsafe_analysis.UnsafeUsageExtractor ./data/json ./results/candidates.csv ./results/intermediate.csv
